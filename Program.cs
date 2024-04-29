4fe4c15e470c3627b4f59b64c024c284649dcbd4
Avduesh Gautam <agt@leapwork.com>
2024-04-29 18:53:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+using OpenQA.Selenium.IE;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var ieOptions = new InternetExplorerOptions();
+        ieOptions.AttachToEdgeChrome = true;
+        ieOptions.InitialBrowserUrl = "https://bing.com";
+        //change the path accordingly
+        ieOptions.EdgeExecutablePath = "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe";
+
+        var driver = new InternetExplorerDriver(ieOptions);
+       
+    }
+}
